30ba760ee3a1315c14d79f9690282f4ed6fb94b7
GabeRundlett <gabe@rundlett.com>
2022-07-06 10:59:17 -0600

    Updated dxc

--- ports/dxc/portfile.cmake
+++ ports/dxc/portfile.cmake
@@ -139,8 +139,8 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 if (VCPKG_TARGET_IS_LINUX)
     # We must modify the support/winadapter.h header to actually be able to compile
     file(READ "${CURRENT_PACKAGES_DIR}/include/dxc/Support/WinAdapter.h" WIN_ADAPTER_H)
-    string(REGEX REPLACE [=[__uuidof\((.)\{\}\)]=] [=[__uuidof(decltype(\1{}))]=] WIN_ADAPTER_H_FIXED ${WIN_ADAPTER_H})
-    file(WRITE "${CURRENT_PACKAGES_DIR}/include/dxc/Support/WinAdapter.h" ${WIN_ADAPTER_H_FIXED})
+    string(REGEX REPLACE [=[__uuidof\((.)\)([^ ])]=] [=[__uuidof(decltype(\1{}))\2]=] WIN_ADAPTER_H_FIXED "${WIN_ADAPTER_H}")
+    file(WRITE "${CURRENT_PACKAGES_DIR}/include/dxc/Support/WinAdapter.h" "${WIN_ADAPTER_H_FIXED}")
 endif()
 
 # Handle copyright (copied verbatim from github.com/microsoft/DirectXShaderCompiler)
