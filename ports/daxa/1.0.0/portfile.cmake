5301fe457f03987253145a7e181115ce66e341a7
GabeRundlett <gabe@rundlett.com>
2022-08-08 03:22:16 -0600

    Updated daxa

--- ports/daxa/1.0.0/portfile.cmake
+++ ports/daxa/1.0.0/portfile.cmake
@@ -3,10 +3,26 @@ vcpkg_from_git(
     URL https://github.com/Ipotrick/Daxa
     REF 0ccedc582729ff0d0032b38ee4ab4dc9f105954a
 )
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    utils WITH_UTILS
+    tests WITH_TESTS
+)
+set(DAXA_DEFINES)
+if(WITH_UTILS)
+    list(APPEND DAXA_DEFINES "-DDAXA_ENABLE_UTILS=true")
+endif()
+if(WITH_TESTS)
+    list(APPEND DAXA_DEFINES "-DDAXA_ENABLE_TESTS=true")
+endif()
+
 vcpkg_configure_cmake(
     SOURCE_PATH "${SOURCE_PATH}"
     PREFER_NINJA
+    OPTIONS ${DAXA_DEFINES}
 )
+
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
