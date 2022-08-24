0c1eabfc3a50fea7af16f39fdb947f7743ea54a9
GabeRundlett <gabe@rundlett.com>
2022-08-24 14:19:34 -0600

    Updated fsr2

--- ports/fsr2/templates/0/portfile.cmake
+++ ports/fsr2/templates/0/portfile.cmake
@@ -30,12 +30,14 @@ if(CMAKE_GENERATOR STREQUAL "Visual Studio 16 2019")
     set(FSR2_VS_VERSION 2019)
 endif()
 
-if(CMAKE_GENERATOR_PLATFORM STREQUAL "x64" OR CMAKE_EXE_LINKER_FLAGS STREQUAL "/machine:x64")
-else()
-    message(FATAL_ERROR "Unsupported target platform - only supporting x64 and Win32 currently")
+if(CMAKE_GENERATOR MATCHES "Visual Studio")
+    if(CMAKE_GENERATOR_PLATFORM STREQUAL "x64" OR CMAKE_EXE_LINKER_FLAGS STREQUAL "/machine:x64")
+    else()
+        message(FATAL_ERROR "Unsupported target platform - only supporting x64 and Win32 currently")
+    endif()
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /Z7")
 endif()
 
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /Z7")
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${CMAKE_HOME_DIRECTORY}/bin/ffx_fsr2_api/)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${CMAKE_HOME_DIRECTORY}/bin/ffx_fsr2_api/)
 add_compile_definitions(_UNICODE)
@@ -109,6 +111,15 @@ install(
     ${CMAKE_INSTALL_DATADIR}/fsr2)
 install(TARGETS ffx_fsr2_api ffx_fsr2_api_vk EXPORT fsr2-targets)
 install(EXPORT fsr2-targets DESTINATION ${CMAKE_INSTALL_DATADIR}/fsr2 NAMESPACE fsr2::)
+# TODO: Install only the files that are necessary:
+#   ffx_assert.h
+#   ffx_error.h
+#   ffx_util.h
+#   ffx_types.h
+#   ffx_fsr2.h
+#   ffx_fsr2_interface.h
+#   ffx_fsr2_common.h
+#   ffx_fsr2_resources.h
 install(DIRECTORY ${PROJECT_SOURCE_DIR} TYPE INCLUDE FILES_MATCHING PATTERN "*.h")
 ]==])
 
@@ -149,63 +160,10 @@ endif()
 target_include_directories(ffx_fsr2_api_vk PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/../shaders/vk>)
 target_include_directories(ffx_fsr2_api_vk PUBLIC ${Vulkan_INCLUDE_DIR})
 
-file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/../shaders/vk)
-
-if(WITH_VOLK)
-    find_package(volk CONFIG REQUIRED)
-    target_link_libraries(ffx_fsr2_api_vk LINK_PUBLIC volk::volk)
-endif()
-
-set(FSR2_AUTO_COMPILE_SHADERS true)
-
-if(FSR2_AUTO_COMPILE_SHADERS)
-    set(FFX_SC_DEPENDENT_TARGET ffx_fsr2_api_vk)
-else()
-    set(FFX_SC_DEPENDENT_TARGET ffx_fsr2_api_vk_shaders)
-    add_custom_target(${FFX_SC_DEPENDENT_TARGET})
-endif()
-
-if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.20.0")
-    cmake_policy(SET CMP0116 OLD)
-endif()
-
-get_filename_component(PASS_SHADER_OUTPUT_PATH ${CMAKE_CURRENT_BINARY_DIR}/../shaders/vk ABSOLUTE)
-
-foreach(PASS_SHADER ${PASS_SHADERS})
-    get_filename_component(PASS_SHADER_FILENAME ${PASS_SHADER} NAME_WE)
-    get_filename_component(PASS_SHADER_TARGET ${PASS_SHADER} NAME_WLE)
-    set(PERMUTATION_HEADER ${PASS_SHADER_OUTPUT_PATH}/${PASS_SHADER_TARGET}_permutations.h)
-
-    if(${PASS_SHADER_FILENAME} STREQUAL "ffx_fsr2_compute_luminance_pyramid_pass")
-        set(FFX_SC_ARGS ${FFX_SC_BASE_ARGS} ${FFX_SC_VK_BASE_ARGS} ${FFX_SC_PERMUTATION_ARGS} -DFFX_HALF=0)
-    else()
-        set(FFX_SC_ARGS ${FFX_SC_BASE_ARGS} ${FFX_SC_VK_BASE_ARGS} ${FFX_SC_PERMUTATION_ARGS} -DFFX_HALF={0,1})
-    endif()
-
-    if(USE_DEPFILE)
-        add_custom_command(
-            OUTPUT ${PERMUTATION_HEADER}
-            COMMAND ${FFX_SC_EXECUTABLE} ${FFX_SC_ARGS} -name=${PASS_SHADER_FILENAME} -I${CMAKE_CURRENT_SOURCE_DIR}/shaders -output=${PASS_SHADER_OUTPUT_PATH} ${PASS_SHADER}
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-            DEPENDS ${PASS_SHADER}
-            DEPFILE ${PERMUTATION_HEADER}.d
-        )
-        list(APPEND PERMUTATION_OUTPUTS ${PERMUTATION_HEADER})
-    else()
-        add_custom_command(
-            OUTPUT ${PERMUTATION_HEADER}
-            COMMAND ${FFX_SC_EXECUTABLE} ${FFX_SC_ARGS} -name=${PASS_SHADER_FILENAME} -I${CMAKE_CURRENT_SOURCE_DIR}/shaders -output=${PASS_SHADER_OUTPUT_PATH} ${PASS_SHADER}
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-            DEPENDS ${PASS_SHADER}
-        )
-        list(APPEND PERMUTATION_OUTPUTS ${PERMUTATION_HEADER})
-    endif()
-endforeach(PASS_SHADER)
-
-add_custom_target(shader_permutations_vk DEPENDS ${PERMUTATION_OUTPUTS})
-add_dependencies(${FFX_SC_DEPENDENT_TARGET} shader_permutations_vk)
-source_group("source" FILES ${VK})
-source_group("shaders" FILES ${SHADERS})
+file(DOWNLOAD "https://github.com/GabeRundlett/fsr2-precompiled-shaders/archive/refs/tags/0.1.0.zip" "${CMAKE_CURRENT_BINARY_DIR}/shaders.zip")
+file(ARCHIVE_EXTRACT INPUT "${CMAKE_CURRENT_BINARY_DIR}/shaders.zip" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/..")
+file(COPY "${CMAKE_CURRENT_BINARY_DIR}/../fsr2-precompiled-shaders-0.1.0/shaders" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/..")
+file(REMOVE_RECURSE "${CMAKE_CURRENT_BINARY_DIR}/../fsr2-precompiled-shaders-0.1.0")
 ]==])
 
 if(WITH_DX12)
