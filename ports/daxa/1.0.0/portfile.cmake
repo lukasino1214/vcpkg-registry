7483b0468bdcfeec95175abddd0326727123bad3
GabeRundlett <gabe@rundlett.com>
2022-10-07 18:19:02 -0600

    Updated daxa

--- ports/daxa/1.0.0/portfile.cmake
+++ ports/daxa/1.0.0/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/Ipotrick/Daxa
-    REF f8cbad15e1c8a393e01fa80c514d06f188ab60ba
+    REF 3f37b175c6fe06a8962e9cf28dc0a0bdabe36380
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
