a497df5b30bb10ac54f3976ad6119932e3ac2648
GabeRundlett <gabe@rundlett.com>
2022-10-16 08:36:05 -0600

    add custom GLFW port

--- update_all.ps1
+++ update_all.ps1
@@ -53,11 +53,10 @@ function update_vcpkg_port() {
     }
 }
 
-update_vcpkg_port    daxa    "0.0.1" "0" packaged              "0.1.0" "1" 0.1.0       "nightly" "1" refs/heads/master
-update_vcpkg_port    dxc     "0.1.2" "0" refs/heads/master
-update_vcpkg_port    fsr2    "2.0.0" "0" refs/tags/v2.0.1a
-
-# "2.1.0" "0" refs/tags/v2.1.0
+update_vcpkg_port    daxa           "0.0.1"  "0" packaged              "0.1.0" "1" 0.1.0       "nightly" "1" refs/heads/master
+update_vcpkg_port    dxc            "0.1.2"  "0" refs/heads/master
+update_vcpkg_port    fsr2           "2.0.0"  "0" refs/tags/v2.0.1a
+update_vcpkg_port    custom-glfw    "custom" "0" refs/heads/master
 
 git pull
 git push
