8ab93956f0221e181a45e4873d2f08070ce35cd6
GitHub Action <github-action@users.noreply.github.com>
2022-11-20 17:32:05 +0000

    Updated daxa

--- ports/daxa/nightly/portfile.cmake
+++ ports/daxa/nightly/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/Ipotrick/Daxa
-    REF 0be4de571ffa824f8af651b0bff53e027c5ce677
+    REF 5a0bacbd8aadedff34219e689cd5ebf156a55dec
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
