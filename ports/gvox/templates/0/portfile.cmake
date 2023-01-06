158030fb54cfc0fddfb60e187a11e8e2727680bf
GabeRundlett <gabe@rundlett.com>
2023-01-06 10:50:23 -0700

    Updated gvox

--- ports/gvox/templates/0/portfile.cmake
+++ ports/gvox/templates/0/portfile.cmake
@@ -6,10 +6,22 @@ vcpkg_from_git(
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
+    file-io WITH_FILE_IO
+    zlib WITH_ZLIB
+    gzip WITH_GZIP
     tests WITH_TESTS
 )
 
 set(GVOX_DEFINES)
+if(WITH_FILE_IO)
+    list(APPEND GVOX_DEFINES "-DGVOX_ENABLE_FILE_IO=true")
+endif()
+if(WITH_ZLIB)
+    list(APPEND GVOX_DEFINES "-DGVOX_ENABLE_ZLIB=true")
+endif()
+if(WITH_GZIP)
+    list(APPEND GVOX_DEFINES "-DGVOX_ENABLE_GZIP=true")
+endif()
 if(WITH_TESTS)
     list(APPEND GVOX_DEFINES "-DGVOX_ENABLE_TESTS=true")
 endif()
